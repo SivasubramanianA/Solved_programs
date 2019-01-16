805033ef5110446aea7fcfb81f6e1de7b9e3b6da
SivasubramanianA <43545784+SivasubramanianA@users.noreply.github.com>
2019-01-16 12:21:45 +0530

    Update prime_or_not.py

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,9 +1,10 @@
 n=int(input())
+j=0
 if n>1:
     for i in range(2,n):
         if n%i==0:
-            print "no"
-    else:
-        print "yes"
+            j=j+1
+if j<=0:
+    print "yes"
 else:
     print "no"
