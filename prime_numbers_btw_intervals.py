eca7957c623840535512c40ae39b662adba39e2c
SivasubramanianA <43545784+SivasubramanianA@users.noreply.github.com>
2019-01-16 12:31:12 +0530

    Create prime_numbers_btw_intervals.py

---
+++ prime_numbers_btw_intervals.py
@@ -0,0 +1,11 @@
+s,e=raw_input().split()
+s=int(s)
+e=int(e)
+for k in range(s+1,e):
+    j=0
+    if k>1:
+        for i in range(2,k):
+            if k%i==0:
+                j=j+1
+    if j<=0:
+       print k
