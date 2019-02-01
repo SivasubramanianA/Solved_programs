d492315edfe6b27e40144b384b73f96d94516335
SivasubramanianA <43545784+SivasubramanianA@users.noreply.github.com>
2019-02-01 21:48:14 +0530

    Create colinear.py

---
+++ colinear.py
@@ -0,0 +1,13 @@
+a,b=raw_input().split()
+c,d=raw_input().split()
+e,f=raw_input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+d=int(d)
+e=int(e)
+f=int(f)
+if a==c==e or b==d==f:
+    print "yes"
+else:
+    print "no"
