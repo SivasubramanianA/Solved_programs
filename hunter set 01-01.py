f89430627bba21d2e3add5821030520fbbec1ec4
SivasubramanianA <43545784+SivasubramanianA@users.noreply.github.com>
2019-03-01 11:14:07 +0530

    Update hunter set 01-01.py

--- hunter set 01-01.py
+++ hunter set 01-01.py
@@ -1,8 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
 l1=[]
-for i in l:
+for i in range(0,n):
 	if(l.count(l[i])!=1):
 		l1.append(l[i])
 s=set(l1)
-print (*s,)
+if len(l1)==0:
+	print ("unique")
+else:
+    print(*s,)
